a3eba96b4e01c08c0c5379e251a70d8417bbdea8
komal8556 <komalmishra8586@gmail.com>
2023-11-06 21:58:54 +0530

    added small changes

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,7 +1,7 @@
 const CallToAction = () => (
   <section className="bg-secondary">
     <div className="py-28 max-w-[86rem] mx-auto px-8 text-center">
-      <h2 className='text-4xl text-textColor font-semibold'>Ready to Streamline Your Links?</h2>
+      <h2 className='text-2xl md:text-4xl text-textColor font-semibold'>Ready to Streamline Your Links?</h2>
       <p className="text-textColor/80 mt-4 text-lg">Experience the Power of LinkSwift Today.</p>
       <button className="mt-6 px-6 py-3 text-lg font-medium text-white bg-accent rounded-2xl hover:bg-[#9a0c4c]">Get Started</button>
     </div>
